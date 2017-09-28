43b4eaee8cec54b52e922d59c4d7f5ed1ac8edfd
jordanstoll23 <jdogstoll@yahoo.com>
2017-09-28 09:27:06 -0700

    Fixed chatBot to better suit stocks

--- server/server.js
+++ server/server.js
@@ -35,26 +35,26 @@ passport.use(new Auth0Strategy({
             db.create_user([profile.displayName, profile.emails[0].value, profile.picture, profile.identities[0].user_id])
             .then(user => {
                 done(null, user[0].id)
-            })
+            });
         }
     }); 
-}))
+}));
 
 passport.serializeUser(function(userID, done){
     done(null, userID);
-})
+});
 passport.deserializeUser(function(userID, done){
     app.get('db').current_user([userID]).then(user => {
-        done(null, user[0])
-    })
-})
+        done(null, user[0]);
+    });
+});
 
 app.get('/auth', passport.authenticate('auth0'));
 
 app.get('/auth/callback', passport.authenticate('auth0',{
     successRedirect: 'http://localhost:3000/#/home',
     failureRedirect: '/auth'
-}))
+}));
 
 app.get('/auth/user', (req, res, next) => {
     if (!req.user){
@@ -63,12 +63,12 @@ app.get('/auth/user', (req, res, next) => {
         return res.status(200).send(req.user);
     }
 
-})
+});
 
 app.get('/auth/logout', (req, res) => {
     req.logOut();
     res.redirect(302, 'http://localhost:3000/')
-})
+});
 
 const PORT = 3535;
 app.listen(PORT, () => console.log('listening on port: ', PORT));
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,37 +9,43 @@ class Review extends Component {
       super(props);
   
       this.state = {
-        name: '',
-        gender: '',
-        age: '',
+        validator: '',
+        NewsPrice: '',
+        startDate: '',
+        endDate: '',
       };
     }
   
     componentWillMount() {
       const { steps } = this.props;
-      const { name, gender, age } = steps;
-  
-      this.setState({ name, gender, age });
+      const { validator, NewsPrice, startDate, endDate } = steps;
+        console.log(this.props)
+      this.setState({ validator, NewsPrice, startDate, endDate});
+      console.log(this.state)
     }
   
     render() {
-      const { name, gender, age } = this.state;
+      const { validator, NewsPrice, startDate, endDate} = this.state;
       return (
         <div style={{ width: '100%' }}>
           <h3>Summary</h3>
           <table>
             <tbody>
               <tr>
-                <td>Name</td>
-                <td>{name.value}</td>
+                <td>stock</td>
+                <td>{validator.value}</td>
               </tr>
               <tr>
-                <td>Gender</td>
-                <td>{gender.value}</td>
+                <td>News/Price</td>
+                <td>{NewsPrice.value}</td>
               </tr>
               <tr>
-                <td>Age</td>
-                <td>{age.value}</td>
+                <td>Start Date</td>
+                <td>{startDate.value}</td>
+              </tr>
+              <tr>
+                <td>End Date</td>
+                <td>{endDate.value}</td>
               </tr>
             </tbody>
           </table>
@@ -77,48 +83,58 @@ class Review extends Component {
             {
               id: '1',
               message: 'Please enter the stock abbreviation you would like to search',
-              trigger: 'name',
+              trigger: 'validator',
             },
             {
-              id: 'name',
+              id:'validator',
               user: true,
               trigger: '3',
-            },
-            {
-              id: '3',
-              message: 'Hi {previousValue}! What is your gender?',
-              trigger: 'gender',
-            },
-            {
-              id: 'gender',
-              options: [
-                { value: 'male', label: 'Male', trigger: '5' },
-                { value: 'female', label: 'Female', trigger: '5' },
-              ],
-            },
-            {
-              id: '5',
-              message: 'How old are you?',
-              trigger: 'age',
-            },
-            {
-              id: 'age',
-              user: true,
-              trigger: '7',
               validator: (value) => {
-                if (isNaN(value)) {
-                  return 'value must be a number';
-                } else if (value < 0) {
-                  return 'value must be positive';
-                } else if (value > 120) {
-                  return `${value}? Come on!`;
-                }
+                if (value === 'Todd') {
+                  return 'Todd(QuestionMark?)';
+                } else if (value === 'your stupid') {
+                  return "you're* Do I have to keep going? ";
+                } else if (value === 'damn') {
+                  return 'Todd would not approve of that language..';
+                } else
   
                 return true;
               },
             },
             {
+              id: '3',
+              message: 'I will gather information on {previousValue}! would you like recent news, or current stock price?',
+              trigger: 'NewsPrice',
+            },
+            {
+              id: 'NewsPrice',
+              options: [
+                { value: 'News', label: 'News', trigger: '7' },
+                { value: 'Price', label: 'Price', trigger: '7' },
+              ],
+            },
+            {
               id: '7',
+              message: 'Please enter the date you want to start tracking price ex. 2017-05-10',
+              trigger: 'startDate'
+            },
+            {
+              id: 'startDate',
+              user: true,
+              trigger: '8',
+            },
+            {
+              id: '8',
+              message:'Please enter the date you want to start tracking price ex. 2017-07-10',
+              trigger: 'endDate',
+            },
+            {
+              id: 'endDate',
+              user: true,
+              trigger:'9'
+            },
+            {
+              id: '9',
               message: 'Great! Check out your summary',
               trigger: 'review',
             },
@@ -130,13 +146,13 @@ class Review extends Component {
             },
             {
               id: 'update',
-              message: 'Would you like to update some field?',
-              trigger: 'update-question',
+              message: 'Is there another stock I can search for you?',
+              trigger: 'secondStock',
             },
             {
-              id: 'update-question',
+              id: 'secondStock',
               options: [
-                { value: 'yes', label: 'Yes', trigger: 'update-yes' },
+                { value: 'yes', label: 'Yes', trigger: '1' },
                 { value: 'no', label: 'No', trigger: 'end-message' },
               ],
             },
@@ -170,7 +186,7 @@ class Review extends Component {
             },
             {
               id: 'end-message',
-              message: 'Thanks! Your data was submitted successfully!',
+              message: 'Thanks! Hope to see you again soon',
               end: true,
             },
           ]}
